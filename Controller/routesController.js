63e7e7d7bc69de18c5b95331dc4a01e3088807d9
Anumjawaid <anumjawaid99@gmail.com>
2023-01-27 00:04:17 +0500

    last

--- Controller/routesController.js
+++ Controller/routesController.js
@@ -11,6 +11,7 @@ const { default: axios } = require('axios');
 let defaultCities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"]
 dotenv.config();
 let API = process.env.appID || "a355251073b74f4899d63723232001"
+let socketUser = []
 
 
 const ReturnRouter = function (socket) {
@@ -21,8 +22,23 @@ const ReturnRouter = function (socket) {
     })
 
     socket.on('click', (msg) => {
-        console.log("socket", socket.id)
-        socket.emit('getSocketId', socket.id);
+        console.log(msg, "messahe")
+        console.log("socket frontend", socket.id)
+        if (msg.msg != null) {
+            let ind = socketUser.find((elem) => elem._id == msg.msg)
+            // console.log(ind, "ind")
+            if (ind != undefined) {
+                       
+            }
+            else {
+                socketUser.push({
+                    '_id':msg.msg,
+                    'cities':msg.cities
+                    })
+            }
+            // console.log('ss1',socketUser)
+        }
+
     });
 
     socket.on('disconnect', async () => {
@@ -31,13 +47,13 @@ const ReturnRouter = function (socket) {
     // Weather Default 
     async function getWeatherDefault() {
 
-        let locationArray = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar", "Hyderabad", 'Ziarat', "Khuzdar", "Sialkot","Faisalabad"]
+        let locationArray = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar", "Hyderabad", 'Ziarat', "Khuzdar", "Sialkot", "Faisalabad"]
         for (let i = 0; i < locationArray.length; i++) {
             let exist = await masterModule.find({ 'Name': locationArray[i] })
             try {
                 let weather = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${API}&q=${locationArray[i]}&aqi=no`)
-                console.log(exist, "exist")
-                console.log(weather.data, "data")
+                // console.log(exist, "exist")
+                // console.log(weather.data, "data")
                 if (exist.length != 0) {
                     const _id = exist[0]._id;
                     const updateactivity = await masterModule.findByIdAndUpdate({ _id },
@@ -55,25 +71,23 @@ const ReturnRouter = function (socket) {
                         detail: { temp: weather.data.current.temp_c, condition: weather.data.current.condition }
                     })
                     let citySave = cityData.save()
-                    console.log(citySave, "city Data save")
+                    // console.log(citySave, "city Data save")
                 }
             }
             catch (e) {
-                console.log(e, "error in axios")
+                console.log(e, "error in axios function get weather default")
             }
         }
 
     }
 
-    // getWeatherDefault()
     async function test(def) {
         const records = await masterModule.find({ 'Name': { $in: def } });
         // console.log(records,"records")
         return records
 
     }
-    let a = test()
-    console.log(a, "a")
+
     async function validateUser(password, hash) {
 
         return await bcrypt
@@ -98,7 +112,7 @@ const ReturnRouter = function (socket) {
         if (result.length != 0) {
             let valid = await validateUser(req.body.data.password, result[0].password)
             valid ?
-                res.send({ message: "valid ", result: result })
+                res.send({ message: "valid ", result: result[0] })
                 :
                 res.send({ message: "Invalid", result: "Password doesnot match" })
         }
@@ -127,7 +141,7 @@ const ReturnRouter = function (socket) {
     });
     //Specific city modal for a user
     routes.post('/getWeather', async (req, res) => {
-
+       console.log("weather")
         // Get a single city
         let city = req.body.q;
         let weather;
@@ -138,7 +152,7 @@ const ReturnRouter = function (socket) {
             let weather = await axios.get(url)
             res.send({
                 'message': "Resolved",
-                result: weather.data
+                 result: weather.data
             })
         }
         catch (e) {
@@ -153,69 +167,86 @@ const ReturnRouter = function (socket) {
     });
     // Prerequisite city name and logged in user id format req.body.data.soso
     routes.post('/addCity', async (req, res) => {
-        let _id=req.body.data._id
+        let _id = req.body.data._id
         const result = await userModule.findById(_id)
         console.log(result, "result")
-        let newCity=[...defaultCities,req.body.data.city]
-        console.log(newCity,"newCity")
+        let newCity = [...defaultCities, req.body.data.city]
+        console.log(newCity, "newCity")
         let city = result.cityList.find(element => element == req.body.data.city)
         if (city != undefined) {
             res.send({ message: 'This City Has ALready Been In your List' });
         }
         else {
-             let cityData=await masterModule.find({'Name':req.body.data.city})
-             if(cityData.length!=0){
+            let cityData = await masterModule.find({ 'Name': req.body.data.city })
+            if (cityData.length != 0) {
                 // Update to Users List
                 const updateactivity = await userModule.findByIdAndUpdate({ _id },
                     {
                         $set: {
-                           cityList:newCity,
-                           cities: await test(newCity)
+                            cityList: newCity,
+                            cities: await test(newCity)
 
                         }
                     },
                     { new: true });
-                    res.send({ message: "valid", result: updateactivity })
+                res.send({ message: "valid", result: updateactivity })
 
-             }
-             else{
+            }
+            else {
                 try {
                     let weather = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${API}&q=${req.body.data.city}&aqi=no`)
                     console.log(weather.data, "data")
-                   
-                        const cityData = new masterModule({
-                            Name: req.body.data.city,
-                            detail: { temp: weather.data.current.temp_c, condition: weather.data.current.condition }
-                        })
-                        let citySave = cityData.save()
-                        console.log(citySave, "city Data save")
-                        const updateactivity = await userModule.findByIdAndUpdate({ _id },
-                            {
-                                $set: {
-                                    cityList:newCity,
-                                    cities: await test(newCity)
-        
-                                }
-                            },
-                            { new: true });
-                            console.log(updateactivity,"update")
-                            res.send({ message: "added", result: updateactivity })
-                    
+
+                    const cityData = new masterModule({
+                        Name: req.body.data.city,
+                        detail: { temp: weather.data.current.temp_c, condition: weather.data.current.condition }
+                    })
+                    let citySave = cityData.save()
+                    console.log(citySave, "city Data save")
+                    const updateactivity = await userModule.findByIdAndUpdate({ _id },
+                        {
+                            $set: {
+                                cityList: newCity,
+                                cities: await test(newCity)
+
+                            }
+                        },
+                        { new: true });
+                    console.log(updateactivity, "update")
+                    res.send({ message: "added", result: updateactivity })
+
                 }
                 catch (e) {
-                    console.log(e, "error in axios")
+                    console.log(e, "error in axios in add city route")
                     res.send({ message: "No such City Exist", result: null })
                 }
 
-             }
+            }
 
         }
 
     })
-   setInterval(()=>getWeatherDefault,8000)
+       setInterval(getWeatherDefault,12000000)
+    
+    //    getWeatherDefault()
+    async function sendAPIUpdate(){
+        if(socketUser.length != 0){
+        for(let i=0;i<socketUser.length;i++){
+            console.log(i,"i")
+           socketUser[i]={
+            '_id':socketUser[i]._id,
+            'cities':socketUser[i].cities,
+            'updatedCities':await test(socketUser[i].cities)
+           }
+           socket.emit(socketUser[i]._id,{data:await test(socketUser[i].cities)})
+        }
 
+        // console.log(socketUser,"socketuser")
+    }
 
-
+    }
+    setInterval(sendAPIUpdate,1300000)
+   
     return routes
 }
 
